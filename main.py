a1b483cde1edbee010bb5e2d3c807d6ea29b2d2c
SamroodAli <samroodali@live.com>
2021-01-04 16:44:27 +0530

    Add:button

--- main.py
+++ main.py
@@ -5,26 +5,38 @@ window = Tk()
 window.minsize(width=300, height=300)
 
 # Entry widget
-entry = Entry(width=30)
+entry = Entry(width=10)
 # Add some text to begin with
 entry.insert(END, string="0")
 # Gets text in entry
-entry.grid(column=2, row=0)
+entry.grid(column=1, row=0)
 
 # Label widget for miles
 label = Label(text="Miles")
-label.grid(column=3, row=0)
+label.grid(column=2, row=0)
 
 # Label widget for text - is  equal to
 label = Label(text="is equal to")
-label.grid(column=1, row=1)
+label.grid(column=0, row=1)
 
 # Label widget for result
 label = Label(text="0")
-label.grid(column=2, row=1)
+label.grid(column=1, row=1)
 
 # Label widget for Km
 label = Label(text="Km")
-label.grid(column=3, row=1)
+label.grid(column=2, row=1)
 # preventing window from closing
+
+
+# Calculate button event callback
+def calculate():
+    print("Do something")
+
+
+# calls calculate() when pressed
+button = Button(text="Click Me", command=calculate)
+button.grid(column=1, row=2)
+
+
 window.mainloop()
